5e102c8dc36376994041d6f8e57562304c96c55f
Clint Doriot <clintdoriot@gmail.com>
2017-02-02 20:06:47 -0500

    testing copying files to

--- build.sh
+++ build.sh
@@ -2,34 +2,38 @@
 
 project="Simple Mobile Placeholder"
 
-echo "Attempting to build $project for iOS"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -silent-crashes \
-  -serial "$UNITY_LICENSE_KEY" \
-  -username "$UNITY_USERNAME" \
-  -password "$UNITY_PASSWORD" \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -executeMethod CommandLineBuild.iOSBuild \
-  -quit
+echo $(pwd) > test.log
+cp test.log $CIRCLE_ARTIFACTS/test2.log
 
-cp $(pwd)/unity.log $CIRCLE_ARTIFACTS/unity-build.log
 
-echo "returning the license"
-
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -returnlicense \
-  -silent-crashes \
-  -serial "$UNITY_LICENSE_KEY" \
-  -username "$UNITY_USERNAME" \
-  -password "$UNITY_PASSWORD" \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -quit
-
-cp $(pwd)/unity.log $CIRCLE_ARTIFACTS/unity-return-key.log
-
-cd Builds/iOS
-xcodebuild -project Unity-iPhone.xcodeproj -scheme Unity-iPhone clean build CONFIGURATION_BUILD_DIR=$CIRCLE_ARTIFACTS
+# echo "Attempting to build $project for iOS"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#   -batchmode \
+#   -silent-crashes \
+#   -serial "$UNITY_LICENSE_KEY" \
+#   -username "$UNITY_USERNAME" \
+#   -password "$UNITY_PASSWORD" \
+#   -logFile $(pwd)/unity.log \
+#   -projectPath $(pwd) \
+#   -executeMethod CommandLineBuild.iOSBuild \
+#   -quit
+#
+# cp $(pwd)/unity.log $CIRCLE_ARTIFACTS/unity-build.log
+#
+# echo "returning the license"
+#
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#   -batchmode \
+#   -returnlicense \
+#   -silent-crashes \
+#   -serial "$UNITY_LICENSE_KEY" \
+#   -username "$UNITY_USERNAME" \
+#   -password "$UNITY_PASSWORD" \
+#   -logFile $(pwd)/unity.log \
+#   -projectPath $(pwd) \
+#   -quit
+#
+# cp $(pwd)/unity.log $CIRCLE_ARTIFACTS/unity-return-key.log
+#
+# cd Builds/iOS
+# xcodebuild -project Unity-iPhone.xcodeproj -scheme Unity-iPhone clean build CONFIGURATION_BUILD_DIR=$CIRCLE_ARTIFACTS
